e519c843245fa51d5e9851a6df60f51363caadf4
adibmannan1 <adibmannan1@gmail.com>
2024-02-22 08:00:57 +0600

    best api fixed

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -11,7 +11,7 @@ const Discover = () => {
     return(
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-                <h2 className="font-bold text-3xl text-white text-left">Explore Songs</h2>
+                <h2 className="font-bold text-3xl text-white text-left">Explore the best Songs</h2>
             </div>
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {data.chart_items?.map((song, i) => (
--- src/redux/services/genius.js
+++ src/redux/services/genius.js
@@ -1,36 +1,30 @@
-import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const geniusApi = createApi({
-    reducerPath: 'geniusApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl : 'https://genius-song-lyrics1.p.rapidapi.com',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '37334518dfmshf3643bd84abc70ap1788cbjsn8ab512155e5f')
-            return headers
-        }
+  reducerPath: 'geniusApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://genius-song-lyrics1.p.rapidapi.com',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '64e68b590dmshbe1e0bff85cc930p17d6b5jsnc9f3144e3200');
+      headers.set('X-RapidAPI-Host', 'genius-song-lyrics1.p.rapidapi.com'); // Add this line
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({
+      query: () => ({
+        url: '/chart/songs/',
+        params: {
+          per_page: '50',
+          page: '1',
+        },
+      }),
     }),
-    endpoints: (builder) => ({
-        getTopCharts: builder.query({
-            query: () => ({
-                url: '/chart/songs/',
-                params: {
-                  per_page: '50',
-                  page: '1',
-                },
-            }),
-        }),
-        getSongDetails: builder.query({query: ({songid}) => `/song/details/?id=${songid}`}),
-        getSongRelated:  builder.query({query: ({songid}) => `/song/recommendations/?id=${songid}`}),
-        getArtistDetails:  builder.query({query: (artistId) => `/artist/songs/?id=${artistId}&per_page=5&page=1`}),
-        getSongsBySearch:  builder.query({query: (searchTerm) => `/search/?q=${searchTerm}&per_page=5&page=1`})
-    })
-})
+    getSongDetails: builder.query({ query: ({ songid }) => `/song/details/?id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/song/recommendations/?id=${songid}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artist/songs/?id=${artistId}&per_page=5&page=1` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/?q=${searchTerm}&per_page=5&page=1` }),
+  }),
+});
 
-export const {useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetSongsBySearchQuery} = geniusApi;
-
-
-
-
-
-
-
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery, useGetSongsBySearchQuery } = geniusApi;
